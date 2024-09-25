c9dc16bba0a20a571a59b0b9af4714168e4b030b
yashirook <yashiro.kentaro@gmail.com>
2024-09-25 14:05:48 +0900

    print build date

--- cmd/version.go
+++ cmd/version.go
@@ -9,13 +9,14 @@ import (
 var (
 	version   = "0.1.0"
 	gitCommit string
+	buildDate string
 )
 
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Print the version number of vaptest",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("vaptest version: %s (commit %s)\n", version, gitCommit)
+		fmt.Printf("vaptest version: %s (commit %s, build at %s)\n", version, gitCommit, buildDate)
 	},
 }
 
